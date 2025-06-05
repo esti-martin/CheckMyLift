1754638617d8846b6834595ea2d2cbc69ea21f58
esti-martin <estibalizmartinborja@gmail.com>
2025-06-05 10:07:24 +0200

    maquetación home corrección

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <header>
-        <Link href="/">
+        <Link to="/">
           <img src="./src/assets/Logo.svg" alt="Logo CheckMyLift" className='logo' />
         </Link>
       </header>
