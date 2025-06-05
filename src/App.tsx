73f9135a45f3e3d679db595d35fbb8c625760767
esti-martin <estibalizmartinborja@gmail.com>
2025-06-05 10:06:34 +0200

    maquetación home

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Button from './components/buttons/Button'
 import MobileNavbar from './components/navbars/MobileNavbar'
 import "./styles/global.css"
-import WarningMessage from './components/warnings/Warning'
-import HeartIcon from './components/icons/Icon'
-import BackButton from './components/buttons/BackButton'
 import ElevatorCard from './components/cards/ElevatorCard'
+import { Link } from 'react-router-dom'
 
 function App() {
-  const [isActive, setIsActive] = useState(false);
-  
 
   return (
     <>
-
+      <header>
+        <Link href="/">
+          <img src="./src/assets/Logo.svg" alt="Logo CheckMyLift" className='logo' />
+        </Link>
+      </header>
         
+      <main>
+        <section className='cardContainer'>
+          <ElevatorCard
+          id=""
+          imageUrl="https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=400&q=80"
+          title="Tarjeta completa"
+          description="Esta tarjeta contiene todos los elementos que pediste."
+          warningType="negative"
+          onPrimaryClick={() => alert("Acción principal")}
+          onSecondaryClick={() => alert("Acción secundaria")}
+        />
 
         <ElevatorCard
           id=""
@@ -28,6 +35,10 @@ function App() {
           onPrimaryClick={() => alert("Acción principal")}
           onSecondaryClick={() => alert("Acción secundaria")}
         />
+        </section>
+        
+      </main>
+        
 
 
       <MobileNavbar />
