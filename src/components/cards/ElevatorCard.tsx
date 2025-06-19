24f18f91f8e035b7d8e3955ebe2a55f9d5ff516b
esti-martin <estibalizmartinborja@gmail.com>
2025-06-19 10:37:54 +0200

    prueba de BASE URL

--- src/components/cards/ElevatorCard.tsx
+++ src/components/cards/ElevatorCard.tsx
@@ -1,10 +1,11 @@
-//import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Button from "../buttons/Button";
 import WarningMessage from "../warnings/Warning";
 import HeartIcon from "../icons/Icon";
 import styles from "./ElevatorCard.module.css";
 import useModal from "../../hooks/useModal";
 import IssueModal from "../modals/IssueModal";
+import ReportIcon from "../icons/ReportIcon";
 
 type CardProps = {
   code: string;
@@ -28,11 +29,11 @@ const ElevatorCard: React.FC<CardProps> = ({
         <article className={styles.card}>
             <section className={styles.imageWrapper}>
                 <img 
-                    src={`assets/stations/${code}.jpg`} 
+                    src={`${import.meta.env.BASE_URL}/assets/stations/${code}.jpg`} 
                     alt={title} 
                     className={styles.image} 
                     //El atributo onError permite mostrar una imagen por defecto si no existe la del code
-                    onError={(e) => { e.currentTarget.src = './assets/stations/empty-box.svg'; }}
+                    onError={(e) => { e.currentTarget.src = `${import.meta.env.BASE_URL}/assets/stations/empty-box.svg`; }}
                 />
                 <section className={styles.favorite}>
                     <HeartIcon id={code} />
@@ -40,27 +41,29 @@ const ElevatorCard: React.FC<CardProps> = ({
             </section>
             <section className={styles.content}>
                 <WarningMessage type={warningType} />
-                <h2 className={styles.title}>{title}</h2>
+                <section className={styles.titleAdvert}>
+                    <h2 className={styles.title}>{title}</h2>
+                    <ReportIcon onClick={openModal} />
+                </section>
                 <section className={styles.lineTime}>
                     <p className={styles.line}><span>{line}</span></p>
                     {/*<p className={styles.time}><span>{timeToNextStation}'</span></p>*/}
                 </section>
                 <section className={styles.actions}>
 
-                    {/* Botón de más información para futuro
                     <Link to={`/elevator/${code}`} className={styles.myLink}>
                         <Button variant="btn-second-s">
                             Más información
                         </Button>
                     </Link>
-                    */}
+                    {/* Botón avisar de una avería
                     <Button 
                         variant="btn-second-s" 
                         onClick={openModal}
                     >
                         Avisar de una avería
                     </Button>
-
+                    */}
                     <Button 
                         variant="btn-princ-s" 
                         onClick={() => {
