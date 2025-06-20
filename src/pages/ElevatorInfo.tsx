3d35850551ae7685e758f6b423c030b0cba918c6
esti-martin <estibalizmartinborja@gmail.com>
2025-06-20 10:13:23 +0200

    ElevatorInfo page

--- src/pages/ElevatorInfo.tsx
+++ src/pages/ElevatorInfo.tsx
@@ -2,8 +2,8 @@ import { useParams } from 'react-router-dom';
 import { useStations } from "../hooks/useStations";
 import BackButton from '../components/buttons/BackButton';
 import WarningMessage from '../components/warnings/Warning';
-import styles from "./ElevatorInfo.module.css"
-
+import styles from "./ElevatorInfo.module.css";
+import stationInfo from "../assets/data/stationInfo.json";
 
 const ElevatorInfo: React.FC = () => {
     const { id } = useParams<{ id: string }>();
@@ -15,15 +15,32 @@ const ElevatorInfo: React.FC = () => {
 
     if (!station) return <p>No se ha encontrado la estación con id: {id}</p>;
 
+    // Busca la info extra en el JSON
+    const extraInfo = stationInfo.stations.find((info: any) => info.code === station.code);
+
     return (
-        <section className='main'>
+        <section className={styles.elevatorContent}>
             <BackButton />
             <section className={styles.elevatorInfo}>
                 {/* Usa el id para cargar datos del ascensor */}
                 <h2>{station.name}</h2>
                 <WarningMessage type={station.issues ? "negative" : "positive"} />
+                <p className={styles.line}><span>{station.line}</span></p>
             </section>
-        </section>
+            {/* Mostrar entradas/accesos si existen */}
+            {extraInfo && extraInfo.exits && extraInfo.exits.length > 0 && (
+                <section>
+                    <h3>Entradas</h3>
+                    <ul>
+                        {extraInfo.exits.map((entrance: any, idx: number) => (
+                            <li key={idx}>
+                                <strong>{entrance.name}:</strong> {entrance.address}
+                            </li>
+                        ))}
+                    </ul>
+                </section>
+            )}
+            </section>
     );
 
 };
