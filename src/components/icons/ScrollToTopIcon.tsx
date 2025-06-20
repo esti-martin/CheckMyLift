f02229861dc4c81f4b5937aecdc27f204e6f8cea
esti-martin <estibalizmartinborja@gmail.com>
2025-06-20 10:37:52 +0200

    animación ScrollToTopIcon

--- src/components/icons/ScrollToTopIcon.tsx
+++ src/components/icons/ScrollToTopIcon.tsx
@@ -1,13 +1,36 @@
+import { useState, useEffect } from 'react';
 import styles from './ScrollToTopIcon.module.css';
 import { LuArrowBigUp } from "react-icons/lu";
 
 const ScrollToTopIcon: React.FC = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.scrollY > 0) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener('scroll', toggleVisibility);
+
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility);
+    };
+  }, []);
+
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
   return (
-    <button className={styles.scrollToTop} onClick={scrollToTop} aria-label="Subir al inicio">
+    <button
+      className={`${styles.scrollToTop} ${isVisible ? styles.visible : ''}`}
+      onClick={scrollToTop}
+      aria-label="Subir al inicio"
+    >
       <LuArrowBigUp />
     </button>
   );
